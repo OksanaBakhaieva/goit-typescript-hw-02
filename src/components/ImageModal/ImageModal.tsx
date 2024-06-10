e7613cbde40244c36f8f615a89920faa48fbccf5
OksanaBakhaieva <obahaeva@gmail.com>
2024-06-10 02:33:44 +0100

    Update ImageModal.tsx

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -14,11 +14,33 @@ interface ImageModalProps {
 }
 
 const ImageModal: FC<ImageModalProps> = ({closeModal, isModalOpen, imgSrc, imgDescription, imgAlt,}) =>{
+   const customStyles: Modal.Styles = {
+    overlay: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: "rgba(0, 0, 0, 0.75)",
+    },
+    content: {
+      padding: `none`,
+      border: `none`,
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      overflow: "hidden",
+    },
+  };
   return (
     <div>
       <Modal
         isOpen={isModalOpen}
         onRequestClose={closeModal}
+        style={customStyles}
         contentLabel="Image Modal"
         className={css.modal}
         overlayClassName={css.overlay}
